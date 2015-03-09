69b3f8058df169cc11a17522dd0dd69629821531
usuario@usuario.com <usuario@usuario.com>
2015-03-09 17:04:23 -0300

    Example with Pool of strings

--- Clase04/src/IngresoPorTeclado.java
+++ Clase04/src/IngresoPorTeclado.java
@@ -7,7 +7,6 @@ public class IngresoPorTeclado {
 		
 		Scanner teclado = new Scanner(System.in);
 		
-		System.out.println("Ingresar información:");
 		
 //		String linea = teclado.nextLine();
 //		
@@ -16,11 +15,31 @@ public class IngresoPorTeclado {
 //		double d = teclado.nextDouble();
 //		
 //		System.out.println(d);
-
 		
+		System.out.println("Verificando pool de strings");
+		
+		String strLocal = "Cadena";
+		String strLocal2 = "Cadena";
+
+		if(strLocal.equals(strLocal2)){
+			System.out.println("Comparando strLocal y strLocal2");
+			System.out.println(strLocal == strLocal2?"Está en el pool":"No está en el pool");
+		}else{
+			System.out.println("Las cadenas son distintas");
+		};
+		
+		
+		System.out.println("Ingresar cadena");
+		String str1 = teclado.nextLine(); 
+		
+		if(strLocal.equals(str1)){
+			System.out.println("Comparando strLocal con cadena ingrsada por teclado");
+			System.out.println(strLocal == str1?"Está en el pool":"No está en el pool");
+		}else{
+			System.out.println("Las cadenas son distintas");
+		};
 		
 		teclado.close();
-		
 	}
 	
 }
