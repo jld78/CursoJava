d20b5b9c7148a08c40e86112eec3d0976df60c71
usuario@usuario.com <usuario@usuario.com>
2015-03-09 14:59:53 -0300

    Add accesors methods

--- Clase04/src/SoloDatos.java
+++ Clase04/src/SoloDatos.java
@@ -14,5 +14,38 @@ public class SoloDatos {
 	public void asignarI(int nuevaI) {
 		i = nuevaI;
 	}
+
+	public double obtenerD() {
+		return d;
+	}
+
+	public void asignarD(double nuevaD) {
+		d = nuevaD;
+	}
+
+	public boolean obtenerB() {
+		return b;
+	}
+
+	public void asignarB(boolean nuevaB) {
+		b = nuevaB;
+	}
+
+	public char obtenerC() {
+		return c;
+	}
+
+	public void asignarC(char nuevaC) {
+		c = nuevaC;
+	}
+
+	public String obtenerS() {
+		return s;
+	}
+
+	public void asignarS(String nuevaS) {
+		s = nuevaS;
+	}
+
 	
 }
--- Clase04/src/SoloDatosTest.java
+++ Clase04/src/SoloDatosTest.java
@@ -1,22 +1,38 @@
-
 public class SoloDatosTest {
 
 	public static void main(String[] args) {
-		
+
 		SoloDatos sd1 = new SoloDatos();
-		
+
 		sd1.asignarI(1);
-		
+
 		System.out.println(sd1.obtenerI());
-		System.out.println(sd1.d);
-		
+
+		sd1.asignarB(true);
+
+		System.out.println(sd1.obtenerB());
+
+		sd1.asignarC('j');
+
+		System.out.println(sd1.obtenerC());
+
+		sd1.asignarD(47.5);
+
+		System.out.println(sd1.obtenerD());
+
+		sd1.asignarS("Cadena");
+
+		System.out.println(sd1.obtenerS());
+
+/*
+
 		SoloDatos sd2 = sd1;
 		sd1 = null;
 		sd2.i = 1;
-		
+
 		int i = 3;
 		System.out.println(i);
-		
+*/
 	}
-	
+
 }
