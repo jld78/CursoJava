6ccbfc09d30e3de005fbebd5ef129379ebf3646b
José Luis Díaz <diazjoseluis78@gmail.com>
2015-03-18 11:37:05 -0300

    Ejercicio finalalizado

--- Clase06/src/AdivinarElNro.java
+++ Clase06/src/AdivinarElNro.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import java.util.Set;
 
-
 public class AdivinarElNro {
 
 	public static void main(String[] args) {
@@ -11,12 +10,12 @@ public class AdivinarElNro {
 		String[] respuesta = null;
 		int intentos = 100;
 
-		for(int i=0; i<nroMaquina.length; i++){
-			nroMaquina[i] = (int) (Math.random()*9+1);
+		for (int i = 0; i < nroMaquina.length; i++) {
+			nroMaquina[i] = (int) (Math.random() * 9 + 1);
 			System.out.print(nroMaquina[i]);
 		}
-		
-		nroMaquina = new int[]{1, 2, 1, 5};
+
+		nroMaquina = new int[] { 1, 2, 1, 5 };
 		for (int i : nroMaquina) {
 			System.out.print(i);
 		}
@@ -27,131 +26,82 @@ public class AdivinarElNro {
 
 		Scanner sc = new Scanner(System.in);
 
-
 		/* Guarda los dígitos ingresados por el usuario */
-		
-		
-		while (!sb.toString().equals("XXXX") && intentos > 0){
-			
+
+		while (!sb.toString().equals("XXXX") && intentos > 0) {
+
 			int[] arrayUser = new int[4];
-			respuesta = new String[]{"","","",""};
+			respuesta = new String[] { "", "", "", "" };
 
 			System.out.println("Quedan " + intentos + " ('q' para salir)");
-			
+
 			sb.setLength(0);
 
 			String entrada = sc.nextLine();
-			
-			if(entrada.equals("q")){
-				intentos=0;
+
+			if (entrada.equals("q")) {
+				intentos = 0;
 				continue;
 			}
 
-			for(int i=0; i<4; i++){
-				arrayUser[i] = Integer.parseInt(String.valueOf(entrada.charAt(i))); 
+			for (int i = 0; i < 4; i++) {
+				arrayUser[i] = Integer.parseInt(String.valueOf(entrada
+						.charAt(i)));
 			}
-			
-			/* Primera pasada marcar X */
-			for(int j=0; j < nroMaquina.length; j++){
-				if(nroMaquina[j]==arrayUser[j]){
-					respuesta[j] = "X";
-				}
-			}
-			
-			/*  
-			 * Segunda pasada _
+
+			/*
 			 * 
-			*/
-			
-			/* En el peor de los casos dos nros repetidos */
-//			int a = 0;
-//			int b = 0;
-//			for(int j=0; j < nroMaquina.length; j++){
-//				for(int k=0; k<nroMaquina.length; k++){
-//					if(j != k && nroMaquina[j] == nroMaquina[k] && !respuesta[j].equals("X") 
-//							&& !respuesta[k].equals("X")){
-//						if(nroMaquina[k] != a){
-//							a = nroMaquina[k];
-//						} else {
-//							b = nroMaquina[k];
-//						}
-//					}
-//				}
-//			}
-			
-			int a=0;
-			int b=0;
-			for(int i=0; i < nroMaquina.length; i++){
-				if(respuesta[i] == null && !respuesta[i].equals("X")){
-					if(a != nroMaquina[i]){
-						a = nroMaquina[i];
-					} else if(b != nroMaquina[i]){
-						b = nroMaquina[i];
-					} else {
-						continue;
+			 * Primera pasada marcar X
+			 */
+			for (int j = 0; j < nroMaquina.length; j++) {
+				boolean procesado = false;
+				if (j > 0) {
+					for (int l = 0; l < j; l++) {
+						if (nroMaquina[l] == nroMaquina[j]) {
+							procesado = true;
+							break;
+						}
 					}
-					for(int j=0; j<arrayUser.length; j++){
-						if(!respuesta[j].equals("X")){
-							if(nroMaquina[i] == arrayUser[j]){
-								respuesta[i] = "_";
-							}
+				}
+
+				for (int k = 0; k < nroMaquina.length; k++) {
+					if (nroMaquina[j] == arrayUser[k]) {
+						if (j == k) {
+							respuesta[j] = "X";
+							continue;
 						} else {
+							if(!respuesta[j].equals("X") && !procesado){
+								respuesta[j] = "_";
+							}
+							
 							continue;
 						}
 					}
 				}
 			}
-
-//			for(int j=0; j < nroMaquina.length; j++){
-//				boolean iteracionActual = false;
-//				for(int i=0; i < nroMaquina.length; i++){
-//					if(nroMaquina[j]==arrayUser[i]){
-//						if (j == i) {
-//							//sb.insert(0, "X");
-//							respuesta[j] = "X";
-//							//iteracionActual = true;
-//						} else {
-//							if(respuesta[j] != "X" && !iteracionActual){
-//								//sb.append("_");
-//								respuesta[j] = "_";
-//							}
-//							//iteracionActual = true;
-//						}
-//						iteracionActual = true;
-//					}	
-//				}
-//			}
-			System.out.println("Fin del bucle");
-			//System.out.println(sb.toString());
-			for(int i=0; i < respuesta.length; i++){
-				if(respuesta[i]!=null){
-					sb.append(respuesta[i]);
-					System.out.print(respuesta[i]);
+			
+			System.out.println("\n\nResultado:");
+			System.out.println("\t'X': dígito y ubìcación correcta, el dígito puede estar repetido y se indica con otra 'X' si está en la posición correcta y no se indica si no lo esta"); 
+			System.out.println("\t'_': digíto correcto y ubic. incorrecta, sólo se indica una vez" );
+			for (int i = 0; i < respuesta.length; i++) {
+				if (respuesta[i] != null) {
+					if(respuesta[i].equals("X")){
+						sb.insert(0, 'X');
+					} else if(respuesta[i].equals("_")){
+						sb.append('_');
+					}
 				}
 			}
-			System.out.println();
-			System.out.println(sb.toString());
+			System.out.println("\n" + sb.toString() + "\n");
 			intentos--;
 		};
-
-
-		/* Verifica si se adivino el nro o no antes de salir */
+		
 		if(sb.toString().equals("XXXX")){
-			System.out.println("Nro correcto");
-		} else {
-			System.out.println("No adivino el nro");
-		}
-
-		for(int i=0; i < respuesta.length; i++){
-			if(respuesta[i]!=null){
-				
-				System.out.print(respuesta[i]);
-			}
+			System.out.println("\n Ha adivinado el número \n");
 		}
 
 		sc.close();
 
 	}
-
 
 }
