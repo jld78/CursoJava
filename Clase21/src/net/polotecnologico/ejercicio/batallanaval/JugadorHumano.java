780e0c99097836500e2467a4c872c643a9f1dc18
José Luis Díaz <diazjoseluis78@gmail.com>
2015-05-06 11:46:26 -0300

    Servidor Batalla naval. Correcciones menores.

--- Clase21/src/net/polotecnologico/ejercicio/batallanaval/JugadorHumano.java
+++ Clase21/src/net/polotecnologico/ejercicio/batallanaval/JugadorHumano.java
@@ -2,12 +2,6 @@ package net.polotecnologico.ejercicio.batallanaval;
 
 public class JugadorHumano extends Jugador {
 
-	public JugadorHumano(int casillas) {
-		super(casillas);
-		// TODO Auto-generated constructor stub
-	}
-
-
 	
 	
 }
--- Clase21/src/net/polotecnologico/ejercicio/batallanaval/JugadorMaquina.java
+++ Clase21/src/net/polotecnologico/ejercicio/batallanaval/JugadorMaquina.java
@@ -2,11 +2,6 @@ package net.polotecnologico.ejercicio.batallanaval;
 
 public class JugadorMaquina extends Jugador {
 
-	public JugadorMaquina(int casillas) {
-		super(casillas);
-		// TODO Auto-generated constructor stub
-	}
-
 
 
 }
